d98a2cd5c8e13dc9ac8f60ccdf473e9faca43420
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2016-10-14 10:10:01 +0100

    created game setup page

--- config/routes.js
+++ config/routes.js
@@ -6,4 +6,7 @@ var rouletteController = require('../controllers/roulette-controller');
 router.route('/')
       .get(rouletteController.index);
 
+router.route('/setup')
+      .get(rouletteController.setup);
+
 module.exports = router;
--- controllers/roulette-controller.js
+++ controllers/roulette-controller.js
@@ -1,8 +1,12 @@
-// INDEX - GET /
 function indexRoulette(req , res) {
 	res.status(200).render('roulette/index');
 }
 
+function setupGame(req , res) {
+	res.status(200).render('roulette/setup');
+}
+
 module.exports = {
-	index: indexRoulette
+	index: indexRoulette,
+	setup: setupGame
 }
