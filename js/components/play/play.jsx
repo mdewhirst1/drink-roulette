86f006f7ef8cdd7340070ebdb0790a8d3c5393f3
Matthew Dewhirst <mdewhirst01@gmail.com>
2016-10-17 14:41:40 +0100

    rusian wheel and method to handle animation end

--- js/components/play/play.jsx
+++ js/components/play/play.jsx
@@ -4,6 +4,7 @@ import Btn from '../general/button.jsx';
 import Nav from '../general/nav.jsx';
 
 import Wheel from './wheel.jsx';
+import RussianWheel from './russianWheel.jsx';
 import Players from './players.jsx';
 
 import settingStore from '../../stores/settingStore.js'
@@ -16,14 +17,24 @@ export default class Play extends Component{
 		this.state = settingStore.getSettings();
 	}
 
+	animationEnd () {
+		console.log("animation ended");
+	}
+
 	render () {
+		var wheel;
+		if (!this.state.russianMode) {
+		  wheel = <Wheel action={this.animationEnd} />;
+		} else {
+		  wheel = <RussianWheel action={this.animationEnd} />;
+		}
 		return (
 			<div>
 				<h1>Play</h1>
 				<Nav />
 				<Players players={this.state.players}/>
 				<p>winning score: {this.state.winScore}</p>
-				<Wheel mode={this.state.russianMode} value="wheel coming soon" />
+				{wheel}
 			</div>
     )
 	}
--- js/components/play/russianWheel.jsx
+++ js/components/play/russianWheel.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Wheel extends Component{
+export default class RussianWheel extends Component{
 	constructor (props) {
 		super(props)
 		this.state = {};
@@ -29,6 +29,16 @@ export default class Wheel extends Component{
 				<div className="wheelContainer" ref='wheel' onClick={() => this.setState({spin: true})}>
 					<div className="wheel1">
 						<img src="./wheel.png" className={spin ? 'spin' : ''}/>
+					</div>
+				</div>
+				<div className="wheelContainer" ref='wheel' onClick={() => this.setState({spin: true})}>
+					<div className="wheel2">
+						<img src="./wheel.png" className={spin ? 'spin2' : ''}/>
+					</div>
+				</div>
+				<div className="wheelContainer" ref='wheel' onClick={() => this.setState({spin: true})}>
+					<div className="wheel2">
+						<img src="./wheel.png" className={spin ? 'spin3' : ''}/>
 					</div>
 				</div>
 			</div>
