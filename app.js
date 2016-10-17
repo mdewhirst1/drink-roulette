a13ff115a7c33b8b4f4c0397948f917af4206915
Matthew Dewhirst <mdewhirst01@gmail.com>
2016-10-17 10:54:52 +0100

    changed switch style and added switches for endless and russian modes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'index.html'));
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
--- js/components/gameSettings/gameSettings.jsx
+++ js/components/gameSettings/gameSettings.jsx
@@ -6,19 +6,35 @@ import Nav from '../general/nav.jsx';
 import settingStore from '../../stores/settingStore.js'
 
 export default class Play extends Component{
+	constructor (props) {
+		super(props);
+		this.state = settingStore.getSettings();
+	}
+
 	handleButton () {
 		settingStore.setSettings('winScore', 15);
 	}
 
+	handleSwitch (event) {
+		let key = event.target.id;
+		settingStore.setSettings(key, !this.state.key);
+	}
+
 	render () {
+
 		return (
 			<div>
 				<h1>Game settings</h1>
 				<Nav />
-				<label className="switch">
-				  <input type="checkbox" />
-				  <div className="slider" />
-				</label>
+				<div className="endlessSwitch">
+					<p>endless mode: </p>
+					<input id="endlessMode" type="checkbox" onClick={this.handleSwitch.bind(this)} defaultChecked={this.state.endlessMode}/>
+				</div>
+				<div className="russianSwitch">
+					<p>russian mode: </p>
+					<input id="russianMode" type="checkbox" onClick={this.handleSwitch.bind(this)} defaultChecked={this.state.russianMode}/>
+				</div>
+				<br />
 				<button onClick={this.handleButton}>set settings</button>
 				<Btn value="Play" link="/play" />
 			</div>
